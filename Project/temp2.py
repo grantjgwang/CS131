efaa2053ae2d1c5065cbd923bc5e750672c4fcbb
Jeng-Gang Wang <jenggang@lnxsrv11.seas.ucla.edu>
2022-06-01 15:40:40 -0700

    clean folder

--- Project/temp2.py
+++
@@ -1,9 +0,0 @@
-import asyncio
-async def main():
-    reader, writer = await asyncio.open_connection('127.0.0.1', 12572)
-    writer.write("IAMAT kiwi.cs.ucla.edu +34.068930-118.445127 1621464827.959498503".encode())
-    data = await reader.readline()
-    print('Received: {}'.format(data.decode()))
-    writer.close()
-if __name__ == '__main__':
-    asyncio.run(main())
